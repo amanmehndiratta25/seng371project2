81f49f1654fccb8a22c024e5ce72d39d13f35542
knowlesc <knowlesc8@gmail.com>
2015-03-14 12:03:32 -0700

    Updated api script, test script for matplotlib

--- stack-exchange-data-stuff.py
+++ stack-exchange-data-stuff.py
@@ -2,24 +2,25 @@ import sys
 import requests 
 import time
 import datetime
-'''
-print "Enter tag to search for: "
-tag = raw_input(">> ")
+import os.path
 
 '''
+	Need numpy http://www.lfd.uci.edu/~gohlke/pythonlibs/#numpy
+	Need matplotlib, requests 
+	Might also need this: http://aka.ms/vcpython27
+'''
+
 do_query = False
 
 months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-years = [2012, 2013, 2014]
+years = [2008, 2009, 2010, 2011, 2012, 2013, 2014]
 days = [1, 2]
-count = 0
 
-			
-
-date2 = datetime.datetime(2015, 3, 1, 0, 0, 0, 0, None)
-utc_seconds = int(time.mktime(date2.timetuple()))
-print datetime.datetime.fromtimestamp(utc_seconds)
-
+'''
+	Returns a list of tuples, each containing a "title", and a start/end time
+	Each tuple is used to provide the start and end times for each query
+	The title is used to check if this query has been done before (to save API calls)
+'''
 def get_dates(tag):
 	dates = []
 	for year in years:
@@ -31,7 +32,11 @@ def get_dates(tag):
 			utc_seconds2 = int(time.mktime(date2.timetuple()))
 			dates.append((title, utc_seconds1, utc_seconds2))
 	return dates
-	
+
+'''
+	Creates an API call based on a tag to search for, a page, and a date range (it doesn't actually perform the call)
+	Page should be 1 for the first call, then increased for the next call if "has_more" in the returned json is true, 
+'''
 def make_query(page_value, from_value, to_value, tag):
 	base = "https://api.stackexchange.com/2.2/search?"
 	page = "page=" + str(page_value)
@@ -45,30 +50,63 @@ def make_query(page_value, from_value, to_value, tag):
 	query = base + page + pagesize + fromdate + todate + order + sort + tagged + site
 	return query
 	
-if do_query:
-	print "performing query" 
-	query = base + page + pagesize + fromdate + todate + order + sort + tagged + site
-	print query
-	print '\n'
-
-	r = requests.get(query)
-	if r.json()['has_more'] == False:
-		count = len(r.json()['items'])
-	
-	f = open("test.txt", "w")
-	f.write(str(r.json()))
-	
+'''
+	check existing file to see what queries have already been done
+'''
+def read_existing(output_file):
+	tags = {}
+	if os.path.isfile(output_file):
+		f = open(output_file, 'r')
+		for line in f:
+			line = line.rstrip('\n').split()
+			tags[line[0]] = line[1]
+		print "Found existing file " + output_file
+		f.close()
+	return tags
+		
 def main():
 	if len(sys.argv) == 2:
 		tag = str(sys.argv[1])
 	else:
 		print "Enter tag to search for: "
 		tag = str(raw_input(">> "))
-	
+		
+	output_file = tag + "-api-counts.txt"
+
+	tags = read_existing(output_file)
+
+	f = open(output_file, 'w')
 	dates = get_dates(tag)
 	
 	for date in dates:
-		print make_query(1, date[1], date[2], tag)
+		page = 1
+		count = 0
+		has_more = True
+		
+		# Only perform a query if it has not already been done
+		
+		if (date[0] not in tags.keys()):
+			while has_more == True:
+				
+				query = make_query(page, date[1], date[2], tag)
+				r = requests.get(query)
+				
+				has_more = r.json()['has_more']
+				count = count + len(r.json()['items'])
+				page = page + 1
+				
+				quota_remaining = str(r.json()['quota_remaining'])
+				print quota_remaining + " API calls remaining"
+				if quota_remaining == 0:
+					print "No API calls remaining, exiting program."
+					sys.exit()
+			towrite = date[0] + " " + str(count)
+			print "New data point " + towrite
+			f.write(towrite + "\n")
+		else:
+			towrite = date[0] + " " + str(tags[date[0]])
+			print "Already found data point " + towrite
+			f.write(towrite + "\n")
 	
 if __name__ == "__main__":
 	main()
