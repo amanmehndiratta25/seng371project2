d909ca3c3242f3ce9c90244a7d67179d592d8b09
knowlesc <knowlesc8@gmail.com>
2015-03-17 20:14:58 -0700

    updated graphs to all use same scale

--- graph-results.py
+++ graph-results.py
@@ -17,6 +17,9 @@ def read_stack(output_file):
 			values = values + [line[1]]
 		print "Found existing file " + output_file
 		f.close()
+	values = [int(i) for i in values]
+	scale = 100.0 / float(max(values))
+	values = [float(i) * scale for i in values]
 	return dates, values
 	
 def read_reddit(output_file):
@@ -33,29 +36,35 @@ def read_reddit(output_file):
 			values = values + [line[1]]
 		print "Found existing file " + output_file
 		f.close()
+	values = [int(i) for i in values]
+	scale = 100.0 / float(max(values))
+	values = [float(i) * scale for i in values]
 	return dates, values
 
 def read_stats(output_file):
-  dates = []
-  values = []
-  if os.path.isfile(output_file):
-    f = open(output_file, 'r')
-    for line in f:
-      line = line.rstrip('\n').split()
-      year = int(line[0].split('-')[-3])
-      month = int(line[0].split('-')[-2])
-      day = int(line[0].split('-')[-1])
+	dates = []
+	values = []
+	if os.path.isfile(output_file):
+		f = open(output_file, 'r')
+		for line in f:
+			line = line.rstrip('\n').split()
+			year = int(line[0].split('-')[-3])
+			month = int(line[0].split('-')[-2])
+			day = int(line[0].split('-')[-1])
 
-      dates = dates + [datetime.datetime(year, month, day)]
-      values = values + [line[1]]
-    print "Found existing file " + output_file
-    f.close()
-  return dates, values
+			dates = dates + [datetime.datetime(year, month, day)]
+			values = values + [line[1]]
+		print "Found existing file " + output_file
+		f.close()
+	values = [int(i) for i in values]
+	scale = 100.0 / float(max(values))
+	values = [float(i) * scale for i in values]
+	return dates, values
 
 x, y = read_stats("gitlog_rails_data.txt")
 plot(x, y)
 x, y = read_stack("ruby-on-rails-stack-api-counts.txt")
 plot(x, y)
 x, y = read_reddit("ruby-on-rails-reddit-data.txt")
-#plot(x, y)
+plot(x, y)
 show()
