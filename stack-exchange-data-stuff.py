27cb5af7071eca35974690c81844e9b7a46ffe2f
knowlesc <knowlesc8@gmail.com>
2015-03-17 18:51:30 -0700

    stack exchange API key, wider date range

--- stack-exchange-data-stuff.py
+++ stack-exchange-data-stuff.py
@@ -5,16 +5,17 @@ import datetime
 import os.path
 
 '''
+	Need Requests (pip install requests[security])
 	Need numpy http://www.lfd.uci.edu/~gohlke/pythonlibs/#numpy
-	Need matplotlib, requests 
+	Need matplotlib, 
 	Might also need this: http://aka.ms/vcpython27
 '''
 
-do_query = False
-
 months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-years = [2008, 2009, 2010, 2011, 2012, 2013, 2014]
-days = [1, 2]
+years = [2010, 2011, 2012, 2013, 2014]
+days = [5, 12]
+
+key = "" # optional stack exchange API key (can be provided via argument)
 
 '''
 	Returns a list of tuples, each containing a "title", and a start/end time
@@ -37,7 +38,7 @@ def get_dates(tag):
 	Creates an API call based on a tag to search for, a page, and a date range (it doesn't actually perform the call)
 	Page should be 1 for the first call, then increased for the next call if "has_more" in the returned json is true, 
 '''
-def make_query(page_value, from_value, to_value, tag):
+def make_query(page_value, from_value, to_value, tag, key):
 	base = "https://api.stackexchange.com/2.2/search?"
 	page = "page=" + str(page_value)
 	pagesize = "&pagesize=100"
@@ -47,7 +48,9 @@ def make_query(page_value, from_value, to_value, tag):
 	sort = "&sort=activity"
 	tagged = "&tagged=" + str(tag)
 	site = "&site=stackoverflow"
-	query = base + page + pagesize + fromdate + todate + order + sort + tagged + site
+	apikey = "&key="+key if key else ""
+	
+	query = base + page + pagesize + fromdate + todate + order + sort + tagged + site + apikey
 	return query
 	
 '''
@@ -65,8 +68,39 @@ def read_existing(output_file):
 	return tags
 		
 def main():
-	if len(sys.argv) == 2:
-		tag = str(sys.argv[1])
+
+	tag = ""
+	key = ""
+	
+	'''
+		Optional arguments:
+			tag=<some tag>
+			key=<api key>
+	'''
+	
+	if len(sys.argv) > 1:
+		for arg in sys.argv[1:]:
+			
+			split_arg = arg.split('=')
+			
+			if len(split_arg) == 2:
+			
+				if split_arg[1] == "":
+					print "invalid argument"
+					sys.exit()
+					
+				if split_arg[0] == "tag":
+					tag = split_arg[1]
+			
+				elif split_arg[0] == "key":
+					key = split_arg[1]
+
+				else:
+					print "invalid argument"
+					sys.exit()
+			else:
+				print "invalid argument"
+				sys.exit()
 	else:
 		print "Enter tag to search for: "
 		tag = str(raw_input(">> "))
@@ -77,7 +111,7 @@ def main():
 
 	f = open(output_file, 'w')
 	dates = get_dates(tag)
-	
+		
 	for date in dates:
 		page = 1
 		count = 0
@@ -88,17 +122,37 @@ def main():
 		if (date[0] not in tags.keys()):
 			while has_more == True:
 				
-				query = make_query(page, date[1], date[2], tag)
+				query = make_query(page, date[1], date[2], tag, key)
 				r = requests.get(query)
 				
-				has_more = r.json()['has_more']
-				count = count + len(r.json()['items'])
-				page = page + 1
-				
-				quota_remaining = str(r.json()['quota_remaining'])
-				print quota_remaining + " API calls remaining"
-				if quota_remaining == 0:
-					print "No API calls remaining, exiting program."
+				try:
+					has_more = r.json()['has_more']
+					count = count + len(r.json()['items'])
+					page = page + 1
+					
+					# if you are being throttled this should prevent most errors
+					try:
+						if int(r.json()['backoff']) > 0:
+							print "backing off for " + r.json()['backoff'] + " seconds"
+							time.sleep(r.json()['backoff'])
+					except KeyError:
+						pass
+					
+					quota_remaining = str(r.json()['quota_remaining'])
+					print quota_remaining + " API calls remaining"
+					if int(quota_remaining) == 0:
+						print "No API calls remaining, exiting program."
+						sys.exit()
+				except:
+					error = "Error performing API call"
+					
+					try:
+						if r.json()['error_message']:
+							error = error + ": " + r.json()['error_message']
+					except KeyError:
+						pass
+						
+					print error
 					sys.exit()
 			towrite = date[0] + " " + str(count)
 			print "New data point " + towrite
