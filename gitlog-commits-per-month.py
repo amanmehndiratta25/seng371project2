cbe9afdc7658757d6ddee18249a522fd3bd65ef2
Ryan McDonald <ryan.mcdonald8@gmail.com>
2015-03-24 13:19:46 -0700

    Added a script to find the number of commits per month for a repository.

---
+++ gitlog-commits-per-month.py
@@ -0,0 +1,36 @@
+import sys
+import subprocess
+
+def main():
+    if len(sys.argv) == 3:
+    	repo_directory = str(sys.argv[1])
+        output_file = str(sys.argv[2])
+    else:
+    	print "Usage: gitlog-commits-per-month.py <repo_directory> <output_file_path>"
+        sys.exit(2)
+
+    # Run git shortlog --since <date> --until <date> | grep -E '^[ ]+\w+' | wc -l
+    # for each month of the year since 2008
+    # CAREFUL: If you use dates in the future, it will have unexpected behaviour.
+    months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    years = [2008, 2009, 2010, 2011, 2012, 2013, 2014]
+
+
+    file = open(output_file, 'w')
+
+    for year in years:
+        for month in months:
+            since = str(year) + "-" + str(month) + "-01"
+
+            until = str(year) + "-" + str(month + 1) + "-01"
+            if month == 12:
+                until = str(year + 1) + "-01-01"
+
+            gitShortlogProcess = subprocess.Popen(("git", "-C", repo_directory, "shortlog", "--since", since, "--until", until), stdout=subprocess.PIPE)
+            grepProcess = subprocess.Popen(("grep", "-E", "^[ ]+\w+"), stdin=gitShortlogProcess.stdout, stdout=subprocess.PIPE)
+            numberOfCommits = subprocess.check_output(("wc", "-l"), stdin=grepProcess.stdout)
+
+            file.write(since + " " + str(numberOfCommits))
+
+if __name__ == "__main__":
+	main()
