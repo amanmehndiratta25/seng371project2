8fdfb134905c975d6e5f15b31c2c379f9878181a
knowlesc <knowlesc8@gmail.com>
2015-04-08 16:41:35 -0700

    added web app to graph the data from the python scripts

---
+++ graph_results/app/GraphResults.py
@@ -0,0 +1,111 @@
+import mpld3
+from matplotlib.pylab import *
+import datetime
+import os 
+import sys
+from HighlightLines import HighlightLines
+
+
+def read_data(data):
+	dates = []
+	values = []
+	values_avg = []
+	
+	for line in data:
+		line = line.rstrip('\n').split()
+		year = int(line[0].split('-')[-2])
+		month = int(line[0].split('-')[-1])
+		day = 1
+		dates = dates + [datetime.datetime(year, month, day)]
+		values = values + [line[1]]
+	values = [int(i) for i in values]
+	scale = 100.0 / float(max(values))
+	values = [float(i) * scale for i in values]
+	for i in range(len(values)):
+		values_avg = values_avg + [float((float(values[i]) + float(values[max(i-1, 0)]) + float(values[min(i+1, len(values) - 1)]))/3.0)]
+	return dates, values, values_avg
+
+def graph_results(datasets):
+	use_avg = False
+		
+	if len(sys.argv) == 2:
+		if sys.argv[1] == "-avg":
+			print "using rolling average"
+			use_avg = True
+
+
+
+
+
+	x_array = []
+	y_array = []
+	label_array = []
+
+	# ====== read data ===== 
+	
+	for label in datasets.keys():
+		x, y, y_avg = read_data([s.strip() for s in datasets[label].splitlines()])
+		if use_avg == True:
+			y = y_avg
+		x_array.append(x)
+		y_array.append(y)
+		label_array.append(label)
+	
+		
+	'''
+	x, y, y_avg = read_data("../rails.txt")
+	label="number of contributors"
+	if use_avg == True:
+		y = y_avg
+
+	x_array.append(x)
+	y_array.append(y)
+	label_array.append(label)
+
+	x, y, y_avg = read_data("../ruby-on-rails-stack-api-counts.txt")
+	label="stack overflow questions created"
+	if use_avg == True:
+		y = y_avg
+		
+	x_array.append(x)
+	y_array.append(y)
+	label_array.append(label)
+
+	x, y, y_avg = read_data("../ruby-on-rails-reddit-data.txt")
+	label="new reddit submissions"
+	if use_avg == True:
+		y = y_avg
+		
+	x_array.append(x)
+	y_array.append(y)
+	label_array.append(label)
+'''
+	# ====== read data ===== 
+		
+	# Draw lines
+	fig = plt.figure() 
+	plt.xlabel('Date')
+	plt.ylabel('Percentage of maximum value')
+	ax = fig.add_subplot(111)
+
+	lines = []
+
+	for i in range(len(x_array)):
+		line = ax.plot(x_array[i], y_array[i], label=label_array[i], lw=5, alpha=0.4)
+		mpld3.plugins.connect(fig, HighlightLines(line))
+
+	min_date = datetime.datetime(2008, 1, 1)
+	max_date = datetime.datetime(2015, 1, 1)
+		
+	ax.plot([min_date, max_date], [100,100], label="100% of maximum value", lw=2, color='k')
+
+	ax.set_yticks([], [])
+	plt.ylim([0, 150])
+	plt.xlim([min_date, max_date])
+
+	ax.grid(color='lightgray', alpha=0.7)
+	ax.legend(loc='upper center', title="", framealpha=0.4)
+
+	html = mpld3.fig_to_html(fig)
+
+	return "<center>" + html
---
+++ graph_results/app/views.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, redirect
+from app import app
+import GraphResults
+
+@app.route('/')
+def index():
+	return render_template('page.html')
+	
+@app.route('/graph', methods=['POST'])
+def graph():
+	
+	datasets = {}
+
+	try:
+		data = request.files['file1']
+		desc = request.form['file1desc']
+		if data and desc:
+			datasets[desc] = data.read()
+	except:
+		pass
+	
+	try:
+		data = request.files['file2']
+		desc = request.form['file2desc']
+		if data and desc:
+			datasets[desc] = data.read()
+	except:
+		pass
+		
+	try:
+		data = request.files['file3']
+		desc = request.form['file3desc']
+		if data and desc:
+			datasets[desc] = data.read()
+	except:
+		pass
+		
+	try:
+		data = request.files['file4']
+		desc = request.form['file4desc']
+		if data and desc:
+			datasets[desc] = data.read()
+	except:
+		pass
+	
+	return GraphResults.graph_results(datasets)
+	
+
