9d01f65a1e42cecdbb2d703beb76c48c2082fc36
knowlesc <knowlesc8@gmail.com>
2015-04-09 13:27:59 -0700

    updated dependencies script

--- check_dependencies.py
+++ check_dependencies.py
@@ -1,25 +1,38 @@
+import sys
+
 try:
     import requests
 except ImportError:
-    print "requests"
+    print "module requests[security] needs to be installed"
+    sys.exit(0)
+
+requests_version = requests.__version__.split('.')
+if requests_version[0] < 3 and requests_version[0] < 6:
+	print "requests needs to be updated"
+        sys.exit(0)
 
 try:
     import praw
 except ImportError:
-    print "praw"
+    print "module praw needs to be installed"
+    sys.exit(0)
 
 try:
     import flask
 except ImportError:
-    print "flask"
+    print "module flask needs to be installed"
+    sys.exit(0)
 
 try:
     import mpld3
 except ImportError:
-    print "mpld3"
+    print "module mpld3 needs to be installed"
+    sys.exit(0)
 
 try:
     import matplotlib
 except ImportError:
-    print "matplotlib"
+    print "module matplotlib needs to be installed"
+    sys.exit(0)
+
 
