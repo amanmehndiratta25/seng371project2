15a6999f5abfc3dda1abd7e43fbe20c9fd19acfd
knowlesc <knowlesc8@gmail.com>
2015-04-09 12:29:43 -0700

    added script to check dependencies

---
+++ check_dependencies.py
@@ -0,0 +1,25 @@
+try:
+    import requests
+except ImportError:
+    print "requests"
+
+try:
+    import praw
+except ImportError:
+    print "praw"
+
+try:
+    import flask
+except ImportError:
+    print "flask"
+
+try:
+    import mpld3
+except ImportError:
+    print "mpld3"
+
+try:
+    import matplotlib
+except ImportError:
+    print "matplotlib"
+
