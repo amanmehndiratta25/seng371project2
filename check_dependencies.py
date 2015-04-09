b9c8394b52d511aa221dc20ec04a74f8703d7c89
knowlesc <knowlesc@gmail.com>
2015-04-09 14:00:50 -0700

    update to check_dependencies script

--- check_dependencies.py
+++ check_dependencies.py
@@ -7,7 +7,7 @@ except ImportError:
     sys.exit(0)
 
 requests_version = requests.__version__.split('.')
-if requests_version[0] < 3 and requests_version[0] < 6:
+if int(requests_version[0]) < 3 and int(requests_version[1]) < 6:
 	print "requests needs to be updated"
         sys.exit(0)
 
@@ -24,15 +24,21 @@ except ImportError:
     sys.exit(0)
 
 try:
-    import mpld3
-except ImportError:
-    print "module mpld3 needs to be installed"
-    sys.exit(0)
-
-try:
     import matplotlib
 except ImportError:
     print "module matplotlib needs to be installed"
+    sys.exit(0)
+
+try:
+    import Tkinter
+except ImportError:
+    print "module _tkinter needs to be installed"
+    print "sudo apt-get install python-tk"
+
+try:
+    import mpld3
+except ImportError:
+    print "module mpld3 needs to be installed"
     sys.exit(0)
 
 
